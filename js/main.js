fca3c05ea87d85aee1268634f7b730ce644598c5
Vladimir <v.igolnikoff@gmail.com>
2021-02-23 18:07:05 +0300

    Добавлена анимация тюбов и булки

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
- 
+  let tubes = document.getElementsByClassName('tube');
+  for(tube of tubes) {
+    tube.addEventListener('click', function(){ this.classList.add('active-tube')})
+  }
+ $(function(){
+   $('.tube').click(function(){
+     $(".hovered-tube").first().removeClass("hovered-tube")
+     $(".active-tube").first().removeClass("active-tube");
+     $(this).children(".upside").first().addClass('active-tube');
+
+    $(".upside").bind("animationend", function(){
+      $(this).removeClass("active-tube");
+    });
+
+    $(".tube").mouseenter(function(){
+      $(this).children(".upside").first().addClass("hovered-tube");
+      $("#burger").animate({"left": $(this).offset().left + "px" }, 1000);
+      });
+    })
+
+
+    $(".tube").mouseleave(function(){
+      $(this).children(".upside").first().removeClass("hovered-tube");
+    })
+ })
