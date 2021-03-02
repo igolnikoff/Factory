90d5707382f10a421540ba137e9278a19bdbdfc7
Vladimir <v.igolnikoff@gmail.com>
2021-03-02 17:53:39 +0300

    Доработали функционал конвейера

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
-  let tubes = document.getElementsByClassName('tube');
-  for(tube of tubes) {
-    tube.addEventListener('click', function(){ this.classList.add('active-tube')})
-  }
- $(function(){
-   $('.tube').click(function(){
-     $(".hovered-tube").first().removeClass("hovered-tube")
-     $(".active-tube").first().removeClass("active-tube");
-     $(this).children(".upside").first().addClass('active-tube');
+$(function(){
+  let angle = 0;
 
-    $(".upside").bind("animationend", function(){
-      $(this).removeClass("active-tube");
-    });
+  let fitting_offset = $(".tube").last().offset().left - $("#burger").offset().left;
+  $("#line-up").css ("left", (- fitting_offset - 50) + "px")
 
-    $(".tube").mouseenter(function(){
-      $(this).children(".upside").first().addClass("hovered-tube");
-      $("#burger").animate({"left": $(this).offset().left + "px" }, 1000);
-      });
-    })
+  $('.tube').click(function(){
+    $(".hovered-tube").first().removeClass("hovered-tube");
+    $(".active-tube").first().removeClass("active-tube");
+    $(this).children(".upside").first().addClass('active-tube');
 
+  });
 
-    $(".tube").mouseleave(function(){
-      $(this).children(".upside").first().removeClass("hovered-tube");
-    })
+  $(".upside").bind("animationend", function(){
+    $(this).removeClass("active-tube");
+  });
+
+  $(".tube").mouseenter(function(){
+    $(this).children(".upside").first().addClass("hovered-tube");
+    let offset = $("#burger").offset().left - $(this).offset().left;
+    $("#burger").animate({"left": "-=" + offset + "px" }, 1000);
+    if (offset<0)
+      angle += 90;
+    else
+      angle -= 90;
+    $(".inner-wheel").css({"transform": "rotateZ("+angle+"deg)"});
+    $("#line-up").animate({"left": "-=" + offset + "px" }, 1000);
+    $("#line-down").animate({"left": "+=" + offset + "px" }, 1000);
+
+  });
+
+  $(".tube").mouseleave(function(){
+    $(this).children(".upside").first().removeClass("hovered-tube");
+  });
  })
