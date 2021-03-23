263571521418fa200f76591256c352b01823448e
Vladimir <v.igolnikoff@gmail.com>
2021-03-23 17:21:41 +0300

    Проверка функционала

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(function(){
       setTimeout(function(){$(tube).animate({"height": "300px"}, 100, function(){
         let component = $(this).parent().children(".component").first();
         setTimeout(function(){
-        let top_offset = $("#burger img").first().offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height")) - component_offsets.get(component.attr("id"));
+        let top_offset = $("#burger img").first().offset().top - component.offset().top - component.height() + component_offsets.get(component.attr("id"));
           let burgerTop =  $("#burger").offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height"));
           console.log(top_offset, burgerTop)
           component.animate({"top": "+=" + top_offset + "px", "width": $("#burger img").width() + "px"}, 500)}, 200);
@@ -30,7 +30,8 @@ $(function(){
           component.css("z-index", level)
           setTimeout(function()
           {
-            let top_offset = $("#burger img").first().offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height")) - component_offsets.get(component.attr("id"));
+            //let top_offset = $("#burger img").first().offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height")) - component_offsets.get(component.attr("id"));
+            // определить положение новой картинки в бургере
             $("#burger").children("img").first().before($("<img>", {"src": $(component).attr("src"), "class": "flat"}).css({"top": top_offset + "px", "z-index": level}))
             level += 1;
             component.css("top", "-70px");
