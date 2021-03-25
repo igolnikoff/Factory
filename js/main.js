6efafe537c30fd14c6ca0cd135acc48e9acc611d
Vladimir <v.igolnikoff@gmail.com>
2021-03-25 17:41:57 +0300

    Оптимизирован код

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ $(function(){
   ]);
 
   let current_components = new Array();
+  let is_game_finished = false;
 
   let fitting_offset = $(".tube").last().offset().left - $("#burger").offset().left;
   $("#line-up").css ("left", (- fitting_offset - 50) + "px")
 
   $('.tube').click(function(){
+    if(is_game_finished) return;
+
     $(".hovered-tube").first().removeClass("hovered-tube");
     $(".active-tube").first().removeClass("active-tube");
     $(this).children(".upside").first().addClass('active-tube');
@@ -23,13 +26,17 @@ $(function(){
 
       setTimeout(function(){$(tube).animate({"height": "300px"}, 100, function(){
         let component = $(this).parent().children(".component").first();
+        if(component.attr("id") == "bread-up")
+        {
+          is_game_finished = true;
+        }
         setTimeout(function(){
         let top_offset = $("#burger img").first().offset().top - component.offset().top + parseInt(component.css("top")) + component_offsets.get(component.attr("id"));
           let burgerTop =  $("#burger").offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height"));
           console.log(top_offset, burgerTop)
           component.animate({"top": "+=" + top_offset + "px", "width": $("#burger img").width() + "px"}, 500)}, 200);
           console.log();
-          component.css("z-index", level)
+          setTimeout(function(){component.css("z-index", level)}, 300);
           setTimeout(function()
           {
             //let top_offset = $("#burger img").first().offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height")) - component_offsets.get(component.attr("id"));
@@ -54,11 +61,10 @@ $(function(){
 
   });
 
-  $(".tube").mouseenter(function(){
-    $(this).children(".upside").first().addClass("hovered-tube");
-    let width_diff = Math.abs($("#burger img").width() - $(this).width());
-    let offset = $("#burger").offset().left - $(this).offset().left + width_diff / 2;
-    $("#burger").animate({"left": ($(this).offset().left + width_diff / 2) + "px" }, 1000);
+  function move_burger(new_position)
+  {
+    let offset = $("#burger img").offset().left - new_position;
+    $("#burger").animate({"left": "-=" + offset + "px" }, 1000);
     if (offset<0)
       angle += 90;
     else
@@ -66,10 +72,21 @@ $(function(){
     $(".inner-wheel").css({"transform": "rotateZ("+angle+"deg)"});
     $("#line-up").animate({"left": "-=" + offset + "px" }, 1000);
     $("#line-down").animate({"left": "+=" + offset + "px" }, 1000);
+  }
+
+
+  $(".tube").mouseenter(function(){
+    if(is_game_finished) return;
+    $(this).children(".upside").first().addClass("hovered-tube");
+    let width_diff = Math.abs($("#burger img").width() - $(this).width());
+    let new_position = $(this).offset().left + width_diff / 2;
+    move_burger(new_position);
+
 
   });
 
   $(".tube").mouseleave(function(){
+    if(is_game_finished) return;
     $(this).children(".upside").first().removeClass("hovered-tube");
   });
  })
