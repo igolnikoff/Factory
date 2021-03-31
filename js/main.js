27a3db96b6ea06e525954f84ac4737ddc8e61618
Vladimir <v.igolnikoff@gmail.com>
2021-03-31 15:40:03 +0300

    Сделана конечный экран

--- js/main.js
+++ js/main.js
@@ -13,6 +13,22 @@ $(function(){
   let is_game_finished = false;
   let is_burger_moving = false;
 
+  let scores = 150;
+  let skills = new Map([
+    [300, "Ты и в правду за ЗОЖ?"],
+    [500, "Для перекуса в самый раз"],
+    [700, "Не дурно, Мизим точно понадобится"],
+    [1000, "Гастрит тебе обеспечен"],
+    [1500, "Толстый, это когда ты делаешь селфи в режиме панорамы"]
+
+  ])
+  let calories = new Map([
+    ["meat", 100],
+    ["salad", 30],
+    ["cheese", 80],
+    ["tomato", 50],
+    ["bread-up", 150]
+  ]);
   let fitting_offset = $(".tube").last().offset().left - $("#burger").offset().left;
   $("#line-up").css ("left", (- fitting_offset - 50) + "px")
 
@@ -23,7 +39,7 @@ $(function(){
     $(".active-tube").first().removeClass("active-tube");
     $(this).children(".upside").first().addClass('active-tube');
     let tube = $(this).children(".upside").first();
-    $(tube).animate({"height": "160px"}, 40).animate({"height": "300px"}, 100, function(){
+    $(tube).animate({"height": "-=100px"}, 200).delay(200).animate({"height": "+=140px"}, 200, function(){
         let component = $(this).parent().children(".component").first();
         if(component.attr("id") == "bread-up")
         {
@@ -31,14 +47,12 @@ $(function(){
         }
 
         let top_offset = $("#burger img").first().offset().top - component.offset().top + parseInt(component.css("top")) + component_offsets.get(component.attr("id"));
-          let burgerTop =  $("#burger").offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height"));
-          component.animate({"top": "+=" + top_offset + "px", "width": $("#burger img").width() + "px"}, 500)}, 200, function()
+          component.animate({"top": "+=" + top_offset + "px", "width": $("#burger img").width() + "px"}, 500, function()
 
           {
             component.css("z-index", level);
 
-            //let top_offset = $("#burger img").first().offset().top - component.offset().top - parseInt(component.css("top")) - parseInt (component.css("height")) - component_offsets.get(component.attr("id"));
-            //
+            scores += calories.get(component.attr("id"));
             let add_offset = component_offsets.get(component.attr("id"));
             for (prev_component of current_components)
             {
@@ -50,7 +64,32 @@ $(function(){
             component.css({"top": "-70px", "z-index": 1, "width": "90%"});
           });
 
-      }).animate({"height": "200px"}, 80);
+      }).delay(200).animate({"height": "200px"}, 80, function(){
+        if (!is_game_finished) return;
+        $("#points-1").text(scores);
+        $("#points-2").text(scores);
+        for (let score_amout of skills.keys())
+
+        {
+          if (scores < score_amout)
+          {
+            $("#skill").text("(" + skills.get(score_amout) + ")");
+            break;
+          }
+        }
+        let new_position = $("body").width() + 50;
+        move_burger(new_position);
+        $(".tube-wrapper").animate ({"opacity": 0}, 200, function(){
+          $(this).css("display", "none");
+          $("#result").css("display", "block");
+          $("#result").animate({"opacity": 1}, 200, function(){
+            $("#burger").css("left", - $("#burger").width() + "px");
+            let new_position = ($("body").width() - $("#score").width()) / 2;
+            move_burger(new_position);
+
+          });
+        });
+      });
 
   });
 
@@ -61,20 +100,17 @@ $(function(){
 
   function move_burger(new_position)
   {
-    if(is_burger_moving) return;
-    is_burger_moving = true;
+
     let offset = $("#burger img").offset().left - new_position;
-    $("#burger").animate({"left": "-=" + offset + "px" }, 1000);
+    $("#burger").stop(true, false).animate({"left": "-=" + offset + "px" }, 1000);
     if (offset<0)
       angle += 90;
     else
       angle -= 90;
     $(".inner-wheel").css({"transform": "rotateZ("+angle+"deg)"});
-    $("#line-up").animate({"left": "-=" + offset + "px" }, 1000);
-    $("#line-down").animate({"left": "+=" + offset + "px" }, 1000, function()
-    {
-      is_burger_moving = false;
-    });
+    $("#line-up").stop(true, false).animate({"left": "-=" + offset + "px" }, 1000);
+    $("#line-down").stop(true, false).animate({"left": "+=" + offset + "px" }, 1000);
+
   }
 
 
